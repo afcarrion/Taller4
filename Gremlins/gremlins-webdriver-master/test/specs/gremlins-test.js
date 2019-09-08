2e5ba061d773f1c529f5f364a779bb5efa26e0dc
afcarrion <af.carrion@uniandes.edu.co>
2019-09-08 14:03:58 -0500

    Implementación de especies gremlins para click y form

--- Gremlins/gremlins-webdriver-master/test/specs/gremlins-test.js
+++ Gremlins/gremlins-webdriver-master/test/specs/gremlins-test.js
@@ -1,34 +1,55 @@
 function loadScript(callback) {
-  var s = document.createElement('script');
-  s.src = 'https://rawgithub.com/marmelab/gremlins.js/master/gremlins.min.js';
+  var s = document.createElement("script");
+  s.src = "https://rawgithub.com/marmelab/gremlins.js/master/gremlins.min.js";
   if (s.addEventListener) {
-    s.addEventListener('load', callback, false);
+    s.addEventListener("load", callback, false);
   } else if (s.readyState) {
     s.onreadystatechange = callback;
   }
   document.body.appendChild(s);
 }
 
+/**
+ * Este método recibe un límite de tiempo en el que debe terminar el ataque de los gremlins
+ *  contra la página y crea una armada de gremlins con el método createHorde()
+ * @param {seg} ttl
+ * @param {llamado a finalizar la prueba} callback
+ */
 function unleashGremlins(ttl, callback) {
   function stop() {
-    horde.stop();
+    horde1.stop(); //adicione 1
     callback();
   }
-  var horde = window.gremlins.createHorde();
+  /*var horde = window.gremlins.createHorde();
   horde.seed(1234);
 
   horde.after(callback);
   window.onbeforeunload = stop;
+  //setTimeout(stop, ttl);
+  horde.unleash();*/
+
+  var horde1 = window.gremlins
+    .createHorde()
+    .gremlin(gremlins.species.formFiller())
+    .gremlin(
+      gremlins.species.clicker().clickTypes(["click"])
+      //.showAction(function(x, y) {})
+    );
+  horde1.seed(1234);
+  horde1.after(callback);
+  window.onbeforeunload = stop;
   setTimeout(stop, ttl);
-  horde.unleash();
+  horde1.unleash();
 }
 
-describe('Monkey testing with gremlins ', function() {
+describe("Monkey testing with gremlins ", function() {
+  it("it should not raise any error", function() {
+    browser.url("/");
+    browser.click("button=Cerrar");
 
-  it('it should not raise any error', function() {
-    browser.url('/');
-    browser.click('button=Cerrar');
-
+    /**
+     *
+     */
     browser.timeoutsAsyncScript(60000);
     browser.executeAsync(loadScript);
 
@@ -36,10 +57,13 @@ describe('Monkey testing with gremlins ', function() {
     browser.executeAsync(unleashGremlins, 50000);
   });
 
+  /**
+   * En este simplemente tomamos todos los logs del navegador y los imprimimos
+   * en el terminal en el que se está corriendo la prueba
+   */
   afterAll(function() {
-    browser.log('browser').value.forEach(function(log) {
-      browser.logger.info(log.message.split(' ')[2]);
+    browser.log("browser").value.forEach(function(log) {
+      browser.logger.info(log.message.split(" ")[2]);
     });
   });
-
 });
